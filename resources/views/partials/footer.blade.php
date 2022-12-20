cc88a8b125725f21265dc3910927ddef4c6737d2
karfaisarra <karfaisarra@gmail.com>
2022-12-20 17:33:27 +0100

    add layouts & partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Links</h3>
+                <ul class="list-unstyled">
+                    <li>Home</li>
+                    <li>trains</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Contact Us</h3>
+                <ul class="list-unstyled">
+                    <li>Home</li>
+                    <li>trains</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Learn more</h3>
+                <ul class="list-unstyled">
+                    <li>Home</li>
+                    <li>trains</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
