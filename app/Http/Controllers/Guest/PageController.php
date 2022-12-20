600db0240a3beb91939bff5eaf7b47e4e99c0f85
karfaisarra <karfaisarra@gmail.com>
2022-12-20 17:34:19 +0100

    add controller & model

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+
+        return view('welcome');
+    }
+    public function trains()
+    {
+        $trains = Train::all();
+        return view('trains', compact('trains'));
+    }
+}
